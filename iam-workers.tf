f7e8fc46d6660c53db237f4284127ee847250afb
karunakar1308 <37843462+karunakar1308@users.noreply.github.com>
2020-09-03 11:46:59 -0400

    Update iam-workers.tf

--- iam-workers.tf
+++ iam-workers.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "demo-node" {
-  name = "terraform-eks-demo-node"
+resource "aws_iam_role" "worker-node" {
+  name = "terraform-eks-worker-node"
 
   assume_role_policy = <<POLICY
 {
@@ -18,22 +18,22 @@ POLICY
 
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "worker-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role = aws_iam_role.demo-node.name
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "worker-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role = aws_iam_role.demo-node.name
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "worker-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role = aws_iam_role.demo-node.name
+  role = aws_iam_role.worker-node.name
 }
 
-resource "aws_iam_instance_profile" "demo-node" {
+resource "aws_iam_instance_profile" "worker-node" {
   name = "terraform-eks-demo"
   role = aws_iam_role.demo-node.name
 }
